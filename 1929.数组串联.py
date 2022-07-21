701ea8cfd1ea36990816efaf992f85cf9b4870a8
VentCat <73477803+dsxksss@users.noreply.github.com>
2022-07-21 22:59:33 +0800

    py:1929

---
+++ 1929.数组串联.py
@@ -0,0 +1,11 @@
+#
+# @lc app=leetcode.cn id=1929 lang=python3
+#
+# [1929] 数组串联
+#
+
+# @lc code=start
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        return nums+nums
+# @lc code=end
