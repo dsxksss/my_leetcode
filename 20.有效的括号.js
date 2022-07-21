4cbb77c09c9739ff00552508b21c2fac7afb2f63
VentCat <73477803+dsxksss@users.noreply.github.com>
2022-07-21 23:47:02 +0800

    js:20

---
+++ 20.有效的括号.js
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=20 lang=javascript
+ *
+ * [20] 有效的括号
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+
+var isValid = function(s) {
+  if (s.length % 2 != 0) return false;
+  const pairs = new Map([[")", "("], ["]", "["], ["}", "{"]]);
+  const stk = [];
+  for (let ch of s) {
+    if (pairs.has(ch)) {
+      if (!stk.length || stk[stk.length - 1] !== pairs.get(ch)) {
+        return false;
+      }
+      stk.pop();
+    } else {
+      stk.push(ch);
+    }
+  }
+  return !stk.length;
+};
+
+// @lc code=end
